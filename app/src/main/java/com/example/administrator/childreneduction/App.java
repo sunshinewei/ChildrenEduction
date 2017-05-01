922b13841299228823b93324cdaf55f71877b5aa
sunshinewei <1436699184@qq.com>
2017-05-01 18:06:34 +0800

    init bmob

--- app/src/main/java/com/example/administrator/childreneduction/App.java
+++ app/src/main/java/com/example/administrator/childreneduction/App.java
@@ -13,6 +13,7 @@ import com.orhanobut.logger.LogLevel;
 import com.orhanobut.logger.Logger;
 import com.umeng.analytics.MobclickAgent;
 
+import cn.bmob.v3.Bmob;
 import cn.jpush.android.api.JPushInterface;
 
 
@@ -37,6 +38,23 @@ public class App extends Application {
         initUmeng();
     }
 
+    private void initBmob(){
+        //第一：默认初始化
+        Bmob.initialize(this, "4f44154c942912226ccb1fb492f4c3eb");
+
+        //第二：自v3.4.7版本开始,设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
+        //BmobConfig config =new BmobConfig.Builder(this)
+        ////设置appkey
+        //.setApplicationId("Your Application ID")
+        ////请求超时时间（单位为秒）：默认15s
+        //.setConnectTimeout(30)
+        ////文件分片上传时每片的大小（单位字节），默认512*1024
+        //.setUploadBlockSize(1024*1024)
+        ////文件的过期时间(单位为秒)：默认1800s
+        //.setFileExpiration(2500)
+        //.build();
+        //Bmob.initialize(config);
+    }
     /**
      * 打印日志
      */
