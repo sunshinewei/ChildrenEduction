7daee5c6b6dd80418b22317c7af4802fc9d645d2
sunshinewei <1436699184@qq.com>
2017-05-21 12:49:22 +0800

    video

---
+++ app/src/main/java/com/example/administrator/childreneduction/ui/coll/fragment/VideoCollFragment.java
@@ -0,0 +1,27 @@
+package com.example.administrator.childreneduction.ui.coll.fragment;
+
+import android.view.View;
+
+import com.example.administrator.childreneduction.R;
+import com.example.administrator.childreneduction.ui.base.BaseFagment;
+
+/**
+ * Created by Administrator on 2017/5/21.
+ */
+
+public class VideoCollFragment extends BaseFagment {
+    @Override
+    public int getLayOutID() {
+        return R.layout.fragment_videocoll;
+    }
+
+    @Override
+    public void initView(View mRootView) {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
---
+++ app/src/main/java/com/example/administrator/childreneduction/ui/vedio/activity/VideoPlayerActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.childreneduction.ui.vedio.activity;
+
+import com.example.administrator.childreneduction.R;
+import com.example.baselibrary.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2017/5/21.
+ */
+
+public class VideoPlayerActivity extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return R.layout.activity_videoplayer;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
